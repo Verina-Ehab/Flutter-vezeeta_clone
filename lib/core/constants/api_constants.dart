cf8fbacec9aacbde614ca40e33b75474f47ef852
Verina-Ehab <rinaehab11@gmail.com>
2024-11-14 13:41:54 +0200

    - svgs, images - file fore vezeeta_clone app client - navigation extension - router class, and routes - colors, and text styles - onboarding screen

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,5 @@
+// ignore_for_file: constant_identifier_names
+const String API_KEY =
+    "PzrwjMFFdssYI6z3sTYrAFpGwUnlnTE0Nft6rnGEsy8O6A0gKvBf6khO";
+
+const String BASE_URL = "https://api.pexels.com/v1/";
